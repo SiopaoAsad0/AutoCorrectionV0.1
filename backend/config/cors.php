5ab9753d6da2f5c9e96958d0a67655e4c0af59d9
Dales, jay Ryan <138736438+SiopaoAsad0@users.noreply.github.com>
2026-07-28 15:58:15 +0800

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,20 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
+   'paths' => ['api/*'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['https://your-actual-vercel-domain.vercel.app'],
+'allowed_headers' => ['*'],
+'supports_credentials' => false,
 
 ];
