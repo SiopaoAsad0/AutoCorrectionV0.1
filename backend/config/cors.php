ff1bc8ff798cd94d57b2cecc47a97ba7f5cd03e5
Dales, jay Ryan <138736438+SiopaoAsad0@users.noreply.github.com>
2026-07-28 16:10:12 +0800

    Update CORS configuration to use patterns

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,11 +2,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://auto-correction-v0-1-i6yy.vercel.app',
-        'https://auto-correction-v0-1-i6yy-evrjbl5fi-siopaoasad0s-projects.vercel.app',
+    'allowed_origins' => [],
+    'allowed_origins_patterns' => [
+        '#^https://auto-correction-v0-1-i6yy.*\.vercel\.app$#',
     ],
-    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
